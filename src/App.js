1edd0e540ed74a1cb21b59edc74587eefead66e1
mar1n <30738216+mar1n@users.noreply.github.com>
2020-02-24 22:24:57 +0000

    list of category

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className='category'>films</button>
+      <button className='category'>people</button>
+      <button className='category'>planets</button>
+      <button className='category'>species</button>
+      <button className='category'>starship</button>
+      <button className='category'>vehicles</button>
+    </>
+    
   );
 }
 
